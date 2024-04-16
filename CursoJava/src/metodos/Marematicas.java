e993b37df6579542f846fb967cad9458206039f0
xoan.xc <xoanalonsofernandez@gmail.com>
2024-04-16 17:32:23 +0200

    Sum_06

--- CursoJava/src/metodos/Marematicas.java
+++ CursoJava/src/metodos/Marematicas.java
@@ -1,5 +1,51 @@
 package metodos;
 
-public class Marematicas {
+import java.util.Random;
+
+public class Marematicas 
+{
+	
+	//Metodo que indica si un numero es primo
+	public static boolean isPrimo(int numero)
+	{
+		for(int i = 2; i<= numero/2; i++) 
+		{
+			if(numero % i == 0)
+			{
+				return false;
+			}
+		}
+		return true;
+		
+		
+	}
+	public static void rellenarArray(int[]matriz, int inferior, int superior) 
+	{
+		Random gen2 = new Random();
+		
+		for(int i = 0; i<matriz.length; i++)
+		{
+			matriz[i] = gen2.nextInt (inferior, superior);
+		}
+	
+	}
+	
+	
+
+//Metodo que calcule la media de un array
+public static double calcularMedia(int[] matriz)
+{
+	int suma = 0;
+	double media = 0;
+	for(int valor : matriz)
+	{
+		//suma = suma + valor;
+		suma += valor;
+		
+		
+	}
+	media = suma/matriz.length;
+	return media;
+}
 
 }
--- CursoJava/src/metodos/PruebaMatematicas.java
+++ CursoJava/src/metodos/PruebaMatematicas.java
@@ -1,9 +1,24 @@
 package metodos;
 
+import java.util.Scanner;
+
 public class PruebaMatematicas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//Prueba método isPrimo
+		int number;
+		System.out.println("Introduzca un número");
+		Scanner lector = new Scanner(System.in);
+		number = lector.nextInt();
+		
+		if(Marematicas.isPrimo(number))
+		{
+			
+		}
+		
+		double media2 = Marematicas.calcularMedia(matriz);
 
 	}
 
