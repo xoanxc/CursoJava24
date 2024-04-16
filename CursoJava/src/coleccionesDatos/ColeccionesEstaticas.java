b5c38fe2e9c2745ad3a954bd9a6894aeae8fb746
xoan.xc <xoanalonsofernandez@gmail.com>
2024-04-16 13:49:27 +0200

    Sum_02

---
+++ CursoJava/src/coleccionesDatos/ColeccionesEstaticas.java
@@ -0,0 +1,20 @@
+package coleccionesDatos;
+
+public class ColeccionesEstaticas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Array: Permite almacenar varios valores del mismo tipo
+		int[] matriz = new int[10]; //Inicializo un array para 10 enteros
+		
+		int[] matriz2 = {3, 4};
+		
+		//Acceder a una posición se indica entre corchetes
+		matriz[0] = 9;
+		
+		
+
+	}
+
+}
