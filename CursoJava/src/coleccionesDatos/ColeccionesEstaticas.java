fb99aa1fd7e1357dc4d6983f3e7aee701a474057
xoan.xc <xoanalonsofernandez@gmail.com>
2024-04-16 13:58:39 +0200

    Sum_04

--- CursoJava/src/coleccionesDatos/ColeccionesEstaticas.java
+++ CursoJava/src/coleccionesDatos/ColeccionesEstaticas.java
@@ -13,6 +13,24 @@ public class ColeccionesEstaticas {
 		//Acceder a una posición se indica entre corchetes
 		matriz[0] = 9;
 		
+		//Aceder a la ultima posición
+		matriz[9] = 5;
+		matriz[matriz.length -1] = 5; //Mas pro
+		
+		//Obtener la capacidad de una matriz
+		int capacidad = matriz.length;
+		
+		//Recorrer un array con un bucle for:
+		
+		for(int i = 0; i< matriz.length; i++)
+		{
+			System.out.print(matriz[i] + " ");
+		}
+		System.out.println();
+		
+		//Llenar el array con números aleatorios entre 1 y 10
+		
+		
 		
 
 	}
