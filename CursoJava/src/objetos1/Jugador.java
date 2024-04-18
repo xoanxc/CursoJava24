242f2500bfed2711bcb54b1dcb60599df29868aa
xoan.xc <xoanalonsofernandez@gmail.com>
2024-04-18 17:21:28 +0200

    Sum_05

---
+++ CursoJava/src/objetos1/Jugador.java
@@ -0,0 +1,23 @@
+package objetos1;
+
+public class Jugador {
+	
+	
+	
+	//Un jugador tiene los siguientes datos: Nombres, Equipo, Dorsal y Valor
+	//Añadir constructor por defecto y construcctor que permite inicializar todos los valores
+	//En la clase Test (main) crear dos jugadores y mostrar info por consola.
+	
+	
+	String Manuel;
+	String Gonzalo;
+	
+	
+	public Jugador()
+	{
+		
+	}
+	
+	
+
+}
--- CursoJava/src/objetos1/TestAlumnos.java
+++ CursoJava/src/objetos1/TestAlumnos.java
@@ -9,8 +9,8 @@ public class TestAlumnos {
 		
 		//Declarar una variable de tipo Alumno
 		Alumno a1 = new Alumno();
-		Alumno a2 = new Alumno("93127384K", "Manuel", "manu@gmail.com", 31, 7.1);
-		Alumno a3 = new Alumno("76219831L", "Diego", "diego@hotmail.com", 41, 8.9);
+		Alumno a2 = new Alumno("71239137L", "a", "qiwd", 12, 9.3);
+		Alumno a3 = new Alumno("98237L", "sdiad", "asmoan", 21, 7.4);
 		
 		a1.dni = "87541234L";
 		a1.edad = 17;
@@ -21,15 +21,22 @@ public class TestAlumnos {
 		System.out.println(a2.dni + " " + a2.edad + " " + a2.media);
 		
 		//Metes notas a los alumnos
-		a1.notas.add(8.0);
-		a1.notas.add(7.2);
-		a2.notas.add(4.3);
-		a2.notas.add(9.4);
-		a3.notas.add(9.9);
-		a3.notas.add(10.0);
+		a1.guardarNota(8.6);
+		a1.guardarNota(5.6);
+		a2.guardarNota(7.4);
+		a2.guardarNota(8.9);
+		a3.guardarNota(9.6);
+		a3.guardarNota(46.7); //No entra, porque la valida
 		
+		
+		System.out.println("La media de a1 es: " + a1.media);
+		System.out.println("La media de a2 es: " + a2.media);
+		System.out.println("La media de a3 es: " + a3.media);
+		
+	
 		
 		
 	}
 
 }
+;
